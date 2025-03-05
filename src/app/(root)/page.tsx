1b11c7645e5a40b6721fb7c991c8b7266dcd2528
Chris Ware <c.p.ware@hotmail.com>
2025-03-05 10:23:49 -0700

    ProductList start

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,18 @@
 import Hero from "@/components/homePage/hero/Hero";
+import sampleData from "../../../db/sample-data";
+import ProductList from "@/components/shared/Product/ProductList/ProductList";
 
 export default function Home() {
+  console.log(sampleData);
+
   return (
     <main>
       <Hero />
+      <ProductList
+        data={sampleData.products}
+        title='Newest Arrivals'
+        limit={4}
+      />
     </main>
   );
 }
---
+++ src/components/shared/Product/ProductList/ProductList.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Link from "next/link";
+import LayoutWrapper from "../../LayoutWrapper";
+import styles from "./ProductList.module.css";
+
+interface Props {
+  data: any;
+  title?: string;
+  limit?: number;
+}
+
+export default function ProductList({ data, title, limit }: Props) {
+
+    const limitedData = limit ? data.slice(0, limit) : data;
+
+  return (
+    <section className={styles.container}>
+      <LayoutWrapper>
+        <div className={styles.top}>
+          <h2 className={styles.title}>{title}</h2>
+          <Link href='/shop' className={styles.shopAll}>
+            Shop All
+          </Link>
+        </div>
+        {data.length > 0 ? (
+          <div className={styles.content}>
+            {limitedData.map((product: any, index: number) => (
+              <div key={index}>{product.name}</div>
+            ))}
+          </div>
+        ) : (
+          <div>
+            <p>No Products Found</p>
+          </div>
+        )}
+      </LayoutWrapper>
+    </section>
+  );
+}
